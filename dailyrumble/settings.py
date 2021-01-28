974f66bedd788de7641c114211889af4671a80c7
Aman <amansinha1729@gamail.com>
2021-01-28 19:07:07 +0530

    final commit i guess

--- dailyrumble/settings.py
+++ dailyrumble/settings.py
@@ -147,7 +147,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.Compressed.ManifestStaticFilesStorage']
+STATICFILES_STORAGE = 'whitenoise.storage.Compressed.ManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
