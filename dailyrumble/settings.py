f2ff05677618e29962884deb835575dd5f1c6bfa
Aman <amansinha1729@gamail.com>
2021-01-29 08:42:47 +0530

    image files

--- dailyrumble/settings.py
+++ dailyrumble/settings.py
@@ -148,7 +148,7 @@ STATICFILES_DIRS = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.Compressed.ManifestStaticFilesStorage'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 MEDIA_URL = '/media/'
 
 MESSAGE_TAGS = {
